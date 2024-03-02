4030de455803e4d58213469f05ab48a1c5e4562a
Sebastian Marines <a01383056@tec.mx>
2024-03-02 02:06:44 -0600

    2:06 AM

--- kirclub/club/api.py
+++ kirclub/club/api.py
@@ -1,10 +1,12 @@
 from ninja import NinjaAPI
 from .models import Post
+from ninja.security import django_auth
+
 
 api = NinjaAPI()
 
 
-@api.post("/vote/{post_id}")
+@api.post("/vote/{post_id}", auth=django_auth)
 def vote(request, post_id: int):
     post = Post.objects.get(id=post_id)
     post.likes_count += 1
